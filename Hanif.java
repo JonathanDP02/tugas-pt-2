d9c33b7ddc6db682f59f24ab49d13fc3a0dea23c
muhammadhanif17 <hanifmuhammad1309@gmail.com>
2024-09-05 16:17:40 +0700

    pp

--- Hanif.java
+++ Hanif.java
@@ -1,8 +1,8 @@
 public class Hanif {
 
     public static void main(String[] args) {
-        System.out.println("Nama  : Muhammad Hanif");
-        System.out.println("NIM   : 244107020118");
+        System.out.println("Nama\t : Muhammad Hanif");
+        System.out.println("NIM\t : 244107020118");
     }
 
 }
