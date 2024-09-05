e5f7a1b1acca7a5829146447cf060b7066c8fd2a
muhammadhanif17 <hanifmuhammad1309@gmail.com>
2024-09-05 15:41:51 +0700

    Biodata Hanif

--- jonathan.java
+++ jonathan.java
@@ -1,7 +1,8 @@
 public class jonathan {
 
     public static void main(String[] args) {
-        
+        System.out.println("Nama  : Muhammad Hanif");
+        System.out.println("NIM   : 244107020118");
     }
 
 }
