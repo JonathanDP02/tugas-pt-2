0d90fdd2ae82d11bd92bbb90b1f679d514b4f330
muhammadhanif17 <hanifmuhammad1309@gmail.com>
2024-09-05 16:16:52 +0700

    pp

--- jonathan.java
+++ jonathan.java
@@ -2,8 +2,7 @@ public class jonathan {
 
     public static void main(String[] args) {
 
-        System.out.println("Nama: Jonathan Dwi Pangestu");
-        System.out.println("NIM: 244107020197");
+            
 
     }
 
